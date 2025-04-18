271100882e02a71b4323e4a3470f8310af9bae62
José Bento <joseluizbpj@gmail.com>
2025-04-18 00:27:08 -0300

    Teste unitario de administrador e correção de referências erradas

--- minimal_apiTestes.Testes/Entidades/AdministradorTeste.cs
+++ minimal_apiTestes.Testes/Entidades/AdministradorTeste.cs
@@ -1,5 +1,25 @@
+using minimal_api.Entidades;
+
 namespace minimal_apiTestes.Testes.Entidades;
 
 public class AdministradorTeste
 {
+    [Fact]
+    public void TestarGetSetDePropriedades()
+    {
+        //Arrange
+        var adm = new Administrador();
+
+        //Act
+        adm.Id = 1;
+        adm.Email = "teste@email.com";
+        adm.Senha = "123456";
+        adm.Perfil = "Adm";
+
+        //Assert
+        Assert.Equal(1, adm.Id);
+        Assert.Equal("teste@email.com", adm.Email);
+        Assert.Equal("123456", adm.Senha);
+        Assert.Equal("Adm", adm.Perfil);
+    }
 }
